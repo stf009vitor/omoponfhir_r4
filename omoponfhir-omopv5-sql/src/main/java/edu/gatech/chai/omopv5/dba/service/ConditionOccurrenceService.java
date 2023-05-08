ce07d1e6834e3de8b0a10dae890414c0d2fe0118
Vitor <stf009@einstein.br>
2023-05-08 11:40:17 -0300

    Fix wrong reference

--- omoponfhir-omopv5-sql/src/main/java/edu/gatech/chai/omopv5/dba/service/ConditionOccurrenceService.java
+++ omoponfhir-omopv5-sql/src/main/java/edu/gatech/chai/omopv5/dba/service/ConditionOccurrenceService.java
@@ -64,15 +64,15 @@ public interface ConditionOccurrenceService extends IService<ConditionOccurrence
 				} else if (columnInfo.equalsIgnoreCase("conditionTypeConcept_concept_id")) {
 //------------NEW--------------------------------------------------------------------------------------------------------------------------
 				} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_text")) {
-					visitOccurrence.set_condition_class_text(rs.getString(columnInfo));
+					conditionOccurrence.set_condition_class_text(rs.getString(columnInfo));
 				} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_code")) {
-					visitOccurrence.set_condition_class_code(rs.getString(columnInfo));
+					conditionOccurrence.set_condition_class_code(rs.getString(columnInfo));
 				} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_system")) {
-					visitOccurrence.set_condition_class_system(rs.getString(columnInfo));
+					conditionOccurrence.set_condition_class_system(rs.getString(columnInfo));
 				} else if (columnInfo.equalsIgnoreCase(alias + "_condition_recorded_datetime")) {
-					visitOccurrence.set_condition_recorded_datetime(rs.getString(columnInfo));
+					conditionOccurrence.set_condition_recorded_datetime(rs.getString(columnInfo));
 				} else if (columnInfo.equalsIgnoreCase(alias + "_condition_severity")) {
-					visitOccurrence.set_condition_severity(rs.getString(columnInfo));
+					conditionOccurrence.set_condition_severity(rs.getString(columnInfo));
 //-----------------------------------------------------------------------------------------------------------------------------------------	
 					Concept conditionTypeConcept = ConceptService._construct(rs, null, "conditionTypeConcept");
 					conditionOccurrence.setConditionTypeConcept(conditionTypeConcept);
@@ -138,15 +138,15 @@ public interface ConditionOccurrenceService extends IService<ConditionOccurrence
 
 //------------NEW--------------------------------------------------------------------------------------------------------------------------
 			} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_text")) {
-				visitOccurrence.set_condition_class_text(rowResult.get(columnInfo).getStringValue());
+				conditionOccurrence.set_condition_class_text(rowResult.get(columnInfo).getStringValue());
 			} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_code")) {
-				visitOccurrence.set_condition_class_code(rowResult.get(columnInfo).getStringValue());
+				conditionOccurrence.set_condition_class_code(rowResult.get(columnInfo).getStringValue());
 			} else if (columnInfo.equalsIgnoreCase(alias + "_condition_class_system")) {
-				visitOccurrence.set_condition_class_system(rowResult.get(columnInfo).getStringValue());
+				conditionOccurrence.set_condition_class_system(rowResult.get(columnInfo).getStringValue());
 			} else if (columnInfo.equalsIgnoreCase(alias + "_condition_recorded_datetime")) {
-				visitOccurrence.set_condition_recorded_datetime(rowResult.get(columnInfo).getStringValue());
+				conditionOccurrence.set_condition_recorded_datetime(rowResult.get(columnInfo).getStringValue());
 			} else if (columnInfo.equalsIgnoreCase(alias + "_condition_severity")) {
-				visitOccurrence.set_condition_severity(rowResult.get(columnInfo).getStringValue());
+				conditionOccurrence.set_condition_severity(rowResult.get(columnInfo).getStringValue());
 //-----------------------------------------------------------------------------------------------------------------------------------------	
 				
 			} else if (columnInfo.equalsIgnoreCase("provider_provider_id")) {
