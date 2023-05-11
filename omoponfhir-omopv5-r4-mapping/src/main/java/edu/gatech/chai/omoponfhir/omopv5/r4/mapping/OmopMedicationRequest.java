05083758743b7a6bb51c718c5a061486cd8f806d
Vitor <stf009@einstein.br>
2023-05-11 09:02:33 -0300

    Med Order

--- omoponfhir-omopv5-r4-mapping/src/main/java/edu/gatech/chai/omoponfhir/omopv5/r4/mapping/OmopMedicationRequest.java
+++ omoponfhir-omopv5-r4-mapping/src/main/java/edu/gatech/chai/omoponfhir/omopv5/r4/mapping/OmopMedicationRequest.java
@@ -321,7 +321,7 @@ public class OmopMedicationRequest extends BaseOmopResource<MedicationRequest, D
 			Coding routeCode = new Coding();
 			List<Coding> routeCodeList = new ArrayList<>();
 
-			routeCode.setDisplay(entity.get_drug_route());
+			routeCode.setDisplay(entity.get_drug_route_source_value());
 			routeCodeList.add(routeCode);
 			codeableConceptRoute.setCoding(routeCodeList);
 			dosage.setRoute(codeableConceptRoute);
